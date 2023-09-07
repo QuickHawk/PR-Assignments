016d294baf3c1e18e6971e5281e5e45de9e60cc5
Aarya <devarlaaarya@gmail.com>
2023-09-07 23:00:16 +0530

    Prediction functionality added

--- 1/app.py
+++ 1/app.py
@@ -1,15 +1,20 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, session
 import pandas
+from flask_session import Session
 import numpy
-from io import BytesIO
+from io import BytesIO, StringIO
 from stats import (
     no_of_features,
     no_of_numerical_features,
     no_of_qualitative_features,
     find_stats_of_all_numerical_columns,
+    predict_gaussian
 )
 
 app = Flask(__name__)
+app.secret_key = "Testing 123"
+app.config["SESSION_TYPE"] = "filesystem"
+Session(app)
 
 def get_statistics(data: pandas.DataFrame):
     stats = {}
@@ -18,13 +23,12 @@ def get_statistics(data: pandas.DataFrame):
     stats["no_of_numerical_features"] = no_of_numerical_features(data)
     stats["no_of_qualitative_features"] = no_of_qualitative_features(data)
 
-    # stats["skew"] = data.select_dtypes(include=[numpy.number]).skew().to_frame().to_html()
-    # stats["kurtosis"] = data.select_dtypes(include=[numpy.number]).kurtosis().to_frame().to_html()
-
-    # stats["other_stats"] = find_stats_of_all_numerical_columns(data).to_html()
     stats["stats"] = find_stats_of_all_numerical_columns(data)
 
-    print(stats)
+    stats["numerical_column_names"] = data.iloc[:,1:].select_dtypes(include=[numpy.number]).columns.values.tolist(),
+
+
+    # print(stats)
 
     return stats
 
@@ -41,16 +45,52 @@ def result_page():
         data_bytes = BytesIO(data_file)
         data = pandas.read_csv(data_bytes)
 
-        # feature_data = data[data.columns.values[1:]]
+        if "data" in session:
+            session.pop("data")
+        session["data"] = data
 
-        stats = get_statistics(data)
+    elif "data" in session:
+        data = session["data"]
+    else:
+        return render_template("error_page.html")
+        
+    stats = get_statistics(data)
+    values = {"sample_data": data.head().to_html(), **stats}
 
-        values = {"sample_data": data.head().to_html(), **stats}
+    if "values" in session:
+        session.pop("values")
 
-        return render_template("result.html", **values)
+    session["values"] = values
+
+    return render_template("result.html", **values)
+
+@app.route("/class-wise")
+def class_wise_distribution():
+    if "values" in session:
+        return render_template("class_wise_distribution.html", **session["values"])
     else:
         return render_template("error_page.html")
 
+@app.route("/predict", methods = ["GET", "POST"])
+def predict():
+    values = {**session["values"]}
+
+    # print("values", values)
+
+    if "data" not in session:
+        return render_template("error_page.html")
+
+    if request.method == "POST":
+        data = session["data"]
+        input_x_string = request.form["input"]
+
+        input_x = pandas.read_csv(StringIO(input_x_string), header=None)
+
+        if not input_x.empty:
+            results = predict_gaussian(data, input_x)
+            values["results"] = results
+
+    return render_template("predict.html", **values)
 
 if __name__ == "__main__":
     app.run(debug=True)
--- 1/stats.py
+++ 1/stats.py
@@ -1,18 +1,18 @@
 import pandas
-import os
 import numpy
+from scipy.stats import multivariate_normal 
 
 def no_of_features(data: pandas.DataFrame) -> int:
     return len(data.columns[1:].values)
 
 def no_of_numerical_features(data: pandas.DataFrame) -> int:
     t = data.iloc[:, 1:].select_dtypes(include=[numpy.number])
-    print(t.columns.values)
+    # print(t.columns.values)
     return len(t.columns.values)
 
 def no_of_qualitative_features(data: pandas.DataFrame) -> int:
     t = data.iloc[:, 1:].select_dtypes(exclude=[numpy.number])
-    print(t.columns.values)
+    # print(t.columns.values)
     return len(t.columns.values)
 
 def find_stats_of_all_numerical_columns(data: pandas.DataFrame):
@@ -20,22 +20,35 @@ def find_stats_of_all_numerical_columns(data: pandas.DataFrame):
     
     label_column_name = data.columns.values[0]
     list_of_labels = data.iloc[:, 0].unique()
-    
+
     for label in list_of_labels:
-        temp_data = data[data[label_column_name] == label]
+        temp_data = data[data[label_column_name] == label].iloc[:, 1:]
         stats[label] = {
             "other": temp_data.describe().to_html(),
             "skew": temp_data.select_dtypes(include=[numpy.number]).skew().to_frame().to_html(),
-            "kurtosis": temp_data.select_dtypes(include=[numpy.number]).kurtosis().to_frame().to_html()
+            "kurtosis": temp_data.select_dtypes(include=[numpy.number]).kurtosis().to_frame().to_html(),
+            "covariance": temp_data.select_dtypes(include=[numpy.number]).cov(ddof=0).to_html(),
+            "correlation": temp_data.select_dtypes(include=[numpy.number]).corr().to_html(),
         }
 
     return stats
 
-if __name__ == "__main__":
-    print(os.path.abspath("."))
-    data = pandas.read_csv("./csv/titanic_data.csv")
-    print(data.columns)
-    print(no_of_features(data))
-    print(no_of_numerical_features(data))
-    print(no_of_qualitative_features(data))
-    print(data.describe())
+def predict_gaussian(data: pandas.DataFrame, x):
+
+    list_of_classes = data.iloc[:, 0].unique()
+    results = {}
+
+    for _class in list_of_classes:
+        class_data = data[data.iloc[:, 0] == _class]
+        class_numeric_data = class_data.select_dtypes(include=[numpy.number])
+
+        class_cov = class_numeric_data.cov(ddof=0)
+        class_mean = class_numeric_data.mean()
+
+        print(x.to_numpy())
+
+        y = multivariate_normal.pdf(x, class_mean, class_cov)
+
+        results[_class] = y
+    
+    return results
