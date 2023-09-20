e9b5fbc7b747d26a19c685a02edda53aff6bbc19
Aarya <devarlaaarya@gmail.com>
2023-09-20 18:18:18 +0530

    Added confusion matrix

--- 1/app.py
+++ 1/app.py
@@ -8,7 +8,11 @@ from stats import (
     no_of_numerical_features,
     no_of_qualitative_features,
     find_stats_of_all_numerical_columns,
-    predict_gaussian
+    predict_gaussian,
+    feature_wise_stats,
+    image_encodings,
+    get_confusion_matrix,
+    confusion_matrix_plot_encoded
 )
 
 app = Flask(__name__)
@@ -19,9 +23,19 @@ Session(app)
 def get_statistics(data: pandas.DataFrame):
     stats = {}
 
-    stats["no_of_features"] = no_of_features(data)
-    stats["no_of_numerical_features"] = no_of_numerical_features(data)
-    stats["no_of_qualitative_features"] = no_of_qualitative_features(data)
+    nFeatures = no_of_features(data)
+    nNumericalFeatures = no_of_numerical_features(data)
+    nQualitativeFeatures = no_of_qualitative_features(data)
+
+    stats["list_of_features"] = nFeatures
+    stats["no_of_features"] = len(nFeatures)
+    stats["list_of_numerical_features"] = nNumericalFeatures
+    stats["no_of_numerical_features"] = len(nNumericalFeatures)
+    stats["list_of_qualitative_features"] = nQualitativeFeatures
+    stats["no_of_qualitative_features"] = len(nQualitativeFeatures)
+
+    stats["labels"] = data.iloc[:,0].unique().astype(str).tolist()
+    stats["n_labels"] = len(stats["labels"])
 
     stats["stats"] = find_stats_of_all_numerical_columns(data)
 
@@ -55,7 +69,7 @@ def result_page():
         return render_template("error_page.html")
         
     stats = get_statistics(data)
-    values = {"sample_data": data.head().to_html(), **stats}
+    values = {"sample_data": data.sample(5).to_html(), **stats}
 
     if "values" in session:
         session.pop("values")
@@ -71,11 +85,29 @@ def class_wise_distribution():
     else:
         return render_template("error_page.html")
 
+@app.route("/feature-wise")
+def feature_wise_distribution():
+    if "data" not in session:
+        return render_template("error_page.html")
+
+    data = session['data']
+    feature_stats = feature_wise_stats(data)
+
+    return render_template("feature_wise_distribution.html", feature_stats=feature_stats)
+
+@app.route("/plots")
+def boxplots():
+    if "data" not in session:
+        return render_template("error_page.html")
+
+    data = session['data']
+    encodings = image_encodings(data)
+
+    return render_template('boxplots.html', encodings = encodings)
+
 @app.route("/predict", methods = ["GET", "POST"])
 def predict():
     values = {**session["values"]}
-
-    # print("values", values)
 
     if "data" not in session:
         return render_template("error_page.html")
@@ -89,6 +121,11 @@ def predict():
         if not input_x.empty:
             results = predict_gaussian(data, input_x)
             values["results"] = results
+
+        conf_matrix, list_of_classes = get_confusion_matrix(data, 0.3)
+        img_data = confusion_matrix_plot_encoded(conf_matrix, list_of_classes)
+
+        values["img_data"] = img_data
 
     return render_template("predict.html", **values)
 
--- 1/stats.py
+++ 1/stats.py
@@ -1,19 +1,26 @@
 import pandas
 import numpy
 from scipy.stats import multivariate_normal 
+from sklearn.metrics import confusion_matrix
+from matplotlib import pyplot
+import matplotlib
+from io import BytesIO
+import base64
+import seaborn
+import math
+
+matplotlib.use('agg')
 
 def no_of_features(data: pandas.DataFrame) -> int:
-    return len(data.columns[1:].values)
+    return data.columns[1:].values
 
 def no_of_numerical_features(data: pandas.DataFrame) -> int:
     t = data.iloc[:, 1:].select_dtypes(include=[numpy.number])
-    # print(t.columns.values)
-    return len(t.columns.values)
+    return t.columns.values
 
 def no_of_qualitative_features(data: pandas.DataFrame) -> int:
     t = data.iloc[:, 1:].select_dtypes(exclude=[numpy.number])
-    # print(t.columns.values)
-    return len(t.columns.values)
+    return t.columns.values
 
 def find_stats_of_all_numerical_columns(data: pandas.DataFrame):
     stats = {}
@@ -45,10 +52,113 @@ def predict_gaussian(data: pandas.DataFrame, x):
         class_cov = class_numeric_data.cov(ddof=0)
         class_mean = class_numeric_data.mean()
 
-        print(x.to_numpy())
-
         y = multivariate_normal.pdf(x, class_mean, class_cov)
 
         results[_class] = y
     
     return results
+
+def feature_wise_stats(data: pandas.DataFrame):
+    numerical_feature_data = data.iloc[:,1:].select_dtypes(include=[numpy.number])
+    
+    label = data.columns.values[0]
+    features = numerical_feature_data.columns.values
+
+    feature_stats = {}
+
+    for feat in features:
+        feature_stats[feat] = data[[label, feat]].groupby(label).describe()
+
+    return feature_stats
+
+def box_plot_encoding(data: pandas.DataFrame):
+    
+    fig, ax = pyplot.subplots()
+
+    data.boxplot(ax=ax, by=data.columns.values[0])
+
+    temp_file = BytesIO()
+    fig.set_figheight(5)
+    fig.set_figwidth(5)
+    fig.savefig(temp_file, format='png')
+    temp_file.seek(0)
+
+    image_encoded = base64.b64encode(temp_file.getvalue()).decode()
+
+    return image_encoded
+
+def confusion_matrix_plot_encoded(conf_matrix, list_of_classes):
+    
+    fig, ax = pyplot.subplots()
+
+    seaborn.heatmap(conf_matrix, annot=True, xticklabels=list_of_classes, yticklabels=list_of_classes, ax=ax)
+
+    pyplot.xlabel("Actual")
+    pyplot.ylabel("Predicted")
+
+    temp_file = BytesIO()
+    fig.set_figheight(5)
+    fig.set_figwidth(5)
+    fig.savefig(temp_file, format='png')
+    temp_file.seek(0)
+
+    image_encoded = base64.b64encode(temp_file.getvalue()).decode()
+
+    return image_encoded
+
+def image_encodings(data: pandas.DataFrame):
+    
+    labels = data.columns.values[0]
+    numerical_features = data.iloc[:,1:].select_dtypes(include=[numpy.number]).columns.values
+
+    encodings = []
+
+    for feature in numerical_features:
+        encodings.append(box_plot_encoding(data[[labels, feature]]))
+
+    return encodings
+    
+def get_confusion_matrix(data: pandas.DataFrame, split_ratio = 0.2):
+
+    train_test_n = math.floor(data.shape[0] * split_ratio)
+
+    shuffled_data = data.sample(frac=1)
+
+    train_data = shuffled_data.iloc[:-train_test_n]
+    test_data = shuffled_data.iloc[-train_test_n:]
+
+    list_of_classes = data.iloc[:, 0].unique().tolist()
+
+    test_x = test_data.iloc[:, 1:].select_dtypes(include=[numpy.number])
+    actual_y = test_y = test_data.iloc[:, 0]
+
+    predicted_y = numpy.zeros((test_x.shape[0], 3))
+
+    label_column_name = data.columns.values[0]
+
+    for idx in range(len(list_of_classes)):
+            
+        _class = list_of_classes[idx]
+
+        train_x = train_data[train_data[label_column_name] == _class].iloc[:, 1:].select_dtypes(include=[numpy.number])
+        train_y = train_data.iloc[:, 0]
+
+        train_mean = train_x.mean()
+        train_cov = train_x.cov(ddof=0)
+
+        predicted_y[:, idx] = multivariate_normal.pdf(test_x, train_mean, train_cov)
+
+    actual_y = [list_of_classes.index(row) for row in actual_y]
+    predicted_y = numpy.argmax(predicted_y, axis=1).tolist()
+
+    print(actual_y)
+    print(predicted_y)
+    # print(actual_y, predicted_y)
+
+    conf_matrix = confusion_matrix(actual_y, predicted_y)
+
+    # pyplot.imshow(conf_matrix)
+
+
+    return conf_matrix, list_of_classes
+    # return [] 
